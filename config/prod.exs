3616dbf51458fdf0ffc2244362865c19d306bc75
Maxime Mouchet <max@maxmouchet.com>
2016-08-07 14:28:30 +0200

    Read port from env in production

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
+config :twisp, http: [port: String.to_integer(System.get_env("PORT") || "4000")]
+
 config :logger, level: :info
--- lib/twisp.ex
+++ lib/twisp.ex
@@ -6,11 +6,12 @@ defmodule Twisp do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    Logger.info "Listening on http://0.0.0.0:4000"
+    port = Application.get_env(:twisp, :http)[:port]
+    Logger.info "Listening on http://0.0.0.0:#{port}"
 
     children = [
       supervisor(Twisp.RecorderSupervisor, []),
-      Plug.Adapters.Cowboy.child_spec(:http, Twisp.Router, [], [port: 4000])
+      Plug.Adapters.Cowboy.child_spec(:http, Twisp.Router, [], [port: port])
     ]
 
     opts = [strategy: :one_for_one, name: Twisp.Supervisor]
